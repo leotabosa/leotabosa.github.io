e5d8ec97323d8d685552335971a04d98eeeda6f5
leotabosa <leeonardotabosa@gmail.com>
2021-01-08 00:07:28 -0300

    credits

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script>
 import Curriculo from "./components/Curriculo";
+import abrirEmNovaAba from "./utils/abrir-em-nova-aba";
 export default {
   components: { Curriculo },
-  methods: {},
+  methods: {
+    abrirEmNovaAba,
+  },
 };
 </script>
 
@@ -10,6 +13,22 @@ export default {
   <div class="container">
     <div class="container__espaco">
       <Curriculo />
+      <div class="container__creditos">
+        This website was heavily inspired by
+        <a
+          class="container__creditos__link"
+          @click="abrirEmNovaAba('https://dribbble.com/shots/7355481-Resume')"
+        >
+          this dribbble
+        </a>
+        by
+        <a
+          class="container__creditos__link"
+          @click="abrirEmNovaAba('https://dribbble.com/webduckdesign')"
+        >
+          webduckdesign.
+        </a>
+      </div>
     </div>
   </div>
 </template>
@@ -26,10 +45,6 @@ export default {
   font-family: -apple-system, BlinkMacSystemFont, "Raleway", "Segoe UI", Roboto,
     Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   border-right: 3px solid var(--cor-fundo);
-  &__espaco {
-    padding: 40px;
-  }
-
   &::-webkit-scrollbar {
     width: 0.45rem;
   }
@@ -39,6 +54,28 @@ export default {
   }
   &::-webkit-scrollbar-track {
     margin: 12px 0;
+  }
+
+  &__espaco {
+    padding: 40px;
+  }
+
+  &__creditos {
+    display: flex;
+    justify-content: center;
+    font-size: 13px;
+    padding: 10px;
+    width: 100%;
+
+    &__link {
+      padding: 0 3px;
+      font-weight: bold;
+      cursor: pointer;
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
   }
 }
 </style>
